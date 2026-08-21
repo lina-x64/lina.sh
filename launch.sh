64bfef4290e131f668357a135782e6b51b8e4668
DAM <43420467+DAMcraft@users.noreply.github.com>
2026-08-21 12:54:29 +0200

    shhh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,4 @@ if [ ! -d "${HOME}/.cache/ms-playwright" ] || [ -z "$(ls -A "${HOME}"/.cache/ms-
     playwright install
 fi
 
-python3.11 -m gunicorn app:app -w 1 --threads 8 --worker-class gevent -b 127.0.0.1:5000 --timeout 120
+python3.13 -m gunicorn app:app -w 1 --threads 8 --worker-class gevent -b 127.0.0.1:5000 --timeout 120
